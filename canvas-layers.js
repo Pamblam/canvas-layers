1fe60aee25d3186c5a028a647a469804fbddae6f
pamblam <adelphia@protonmail.ch>
2021-06-28 11:32:13 -0400

    post build

--- canvas-layers.js
+++ canvas-layers.js
@@ -1,5 +1,5 @@
 /**
- * canvas-layers - v2.0.0
+ * canvas-layers - v2.0.1
  * Allow user to position and re-arrange images on a canvas.
  * @author Pamblam
  * @website 
@@ -10,7 +10,7 @@
 /**
  * Interface for handling all canvas functionality
  * @see https://pamblam.github.io/canvas-layers/examples/
- * @version 2.0.0
+ * @version 2.0.1
  */
 class Canvas{
 	
@@ -1115,7 +1115,7 @@ class Canvas{
  * The version of the library
  * @type {String}
  */
-Canvas.version = '2.0.0';
+Canvas.version = '2.0.1';
 
 /**
  * The default anchorRadius value for all Canvas instances.
@@ -1760,6 +1760,618 @@ class DrawingCanvas extends Canvas{
 		this.drawing_layer = null;
 		this.layer_dimensions = null;
 		this.freehand_coords = [];
+	}
+	
+}
+
+class CanvasFonts{}
+
+CanvasFonts.getNative = async (force_recheck = false) => {
+	
+	if(CanvasFonts._native_available.length && !force_recheck){
+		return CanvasFonts._native_available;
+	}
+
+	var _native_available = [];
+	await document.fonts.ready;
+	CanvasFonts._native_fontlist.forEach(font => {
+		if (document.fonts.check(`12px "${font}"`)) {
+			_native_available.push(font);
+		}
+	})
+
+	CanvasFonts._native_available = [...new Set(_native_available)].sort();
+	return CanvasFonts._native_available;
+};
+
+CanvasFonts.getDownloaded = async (force_recheck = false) => {
+	if(CanvasFonts._downloaded_available.length && !force_recheck){
+		return CanvasFonts._downloaded_available;
+	}
+	
+	let {fonts} = document;
+	const it = fonts.entries();
+
+	let arr = [];
+	let done = false;
+
+	while (!done) {
+		const font = it.next();
+		if (!font.done) {
+			
+			var family;
+			try{
+				family = JSON.parse(font.value[0].family);
+			}catch(e){
+				family = font.value[0].family;
+			}
+			
+			arr.push(family);
+		} else {
+			done = font.done;
+		}
+	}
+	
+	CanvasFonts._downloaded_available = [...new Set(arr)].sort();
+	return CanvasFonts._downloaded_available;
+};
+
+CanvasFonts.getAvailable = async () => {
+	var native = await CanvasFonts.getNative();
+	var downloaded = await CanvasFonts.getDownloaded();
+	return [...new Set([...native, ...downloaded])].sort();
+};
+
+CanvasFonts._native_available = [];
+CanvasFonts._downloaded_available = [];
+CanvasFonts._native_fontlist = [
+	'Arial',
+	'Arial Unicode MS',
+	'Bitstream Cyberbit',
+	'BitstreamCyberCJK',
+	'Brampton',
+	'Cardo',
+	'Caslon Roman',
+	'Charis SIL',
+	'Chrysanthi Unicode',
+	'Chryſanþi Unicode',
+	'ClearlyU',
+	'Code2000',
+	'DejaVu Sans',
+	'Doulos SIL',
+	'Everson Mono',
+	'Gentium Regular',
+	'Gentium Plus',
+	'GNU FreeFont',
+	'Unifont',
+	'GNU Unifont',
+	'HAN NOM A',
+	'HAN NOM B',
+	'Horta',
+	'Junicode',
+	'Kelvinch',
+	'Linux Libertine',
+	'Lucida Grande',
+	'Lucida Sans Unicode',
+	'Microsoft JhengHei',
+	'Microsoft Sans Serif',
+	'New Gulim',
+	'Noto',
+	'PragmataPro',
+	'Quivira',
+	'Segoe UI Regular',
+	'Squarish Sans CT',
+	'STIX',
+	'Sun-ExtA',
+	'Sun-ExtB',
+	'Tahoma',
+	'Times New Roman',
+	'TITUS Cyberbit Basic',
+	'WenQuanYi Bitmap Song',
+	'WenQuanYi Micro Hei',
+	'WenQuanYi Zen Hei',
+	'Y.OzFontN',
+	'XITS',
+	'Aharoni',
+	'Aldhabi',
+	'Andalus',
+	'Angsana New',
+	'AngsanaUPC',
+	'Aparajita',
+	'Arabic Typesetting',
+	'Bahnschrift',
+	'Batang',
+	'BatangChe',
+	'BIZ UDGothic',
+	'BIZ UDPGothic',
+	'BIZ UDMincho',
+	'BIZ UDPMincho',
+	'Book Antiqua',
+	'Browallia New',
+	'BrowalliaUPC',
+	'Calibri',
+	'Calisto MT',
+	'Cambria',
+	'Cambria Math',
+	'Candara',
+	'Century Gothic',
+	'Comic Sans MS',
+	'Consolas',
+	'Constantia',
+	'Copperplate Gothic',
+	'Corbel',
+	'Cordia New',
+	'CordiaUPC',
+	'Courier New',
+	'DaunPenh',
+	'David',
+	'DengXian',
+	'DilleniaUPC',
+	'DFKai-SB',
+	'DokChampa',
+	'Dotum',
+	'DotumChe',
+	'Ebrima',
+	'Estrangelo Edessa',
+	'EucrosiaUPC',
+	'Euphemia',
+	'FangSong',
+	'Franklin Gothic',
+	'FrankRuehl',
+	'FreesiaUPC',
+	'Gabriola',
+	'Gadugi',
+	'Gautami',
+	'Georgia',
+	'Gisha',
+	'Gulim',
+	'GulimChe',
+	'Gungsuh',
+	'GungsuhChe',
+	'HoloLens MDL2 Assets',
+	'Impact',
+	'Ink Free',
+	'IrisUPC',
+	'Iskoola Pota',
+	'JasmineUPC',
+	'Javanese Text',
+	'SimKai',
+	'KaiTi',
+	'Kalinga',
+	'Kartika',
+	'Khmer UI',
+	'KodchiangUPC',
+	'Kokila',
+	'Lao UI',
+	'Latha',
+	'Leelawadee',
+	'Leelawadee UI',
+	'Levenim MT',
+	'LilyUPC',
+	'Lucida Console',
+	'Lucida Handwriting',
+	'Malgun Gothic',
+	'Mangal',
+	'Marlett',
+	'Meiryo',
+	'Meiryo UI',
+	'Microsoft Himalaya',
+	'Microsoft JhengHei UI',
+	'Microsoft New Tai Lue',
+	'Microsoft PhagsPa',
+	'Microsoft Tai Le',
+	'Microsoft Uighur',
+	'Microsoft YaHei',
+	'Microsoft YaHei UI',
+	'Microsoft Yi Baiti',
+	'MingLiU',
+	'PMingLiU',
+	'MingLiU-ExtB',
+	'PMingLiU-ExtB',
+	'MingLiU_HKSCS',
+	'MingLiU_HKSCS-ExtB',
+	'Miriam',
+	'Miriam Fixed',
+	'Mongolian Baiti',
+	'MoolBoran',
+	'MS Gothic',
+	'MS PGothic',
+	'MS Mincho',
+	'MS PMincho',
+	'MS UI Gothic',
+	'MV Boli',
+	'Myanmar Text',
+	'Narkisim',
+	'Nirmala UI',
+	'NSimSun',
+	'Nyala',
+	'Palatino Linotype',
+	'Plantagenet Cherokee',
+	'Raavi',
+	'Rod',
+	'Sakkal Majalla',
+	'Sanskrit Text',
+	'Segoe MDL2 Assets',
+	'Segoe Print',
+	'Segoe Script',
+	'Segoe SD',
+	'Segoe UI',
+	'Segoe UI Emoji',
+	'Segoe UI Historic',
+	'Segoe UI Symbol',
+	'Shonar Bangla',
+	'Shruti',
+	'SimHei',
+	'Simplified Arabic',
+	'SimSun',
+	'SimSun-ExtB',
+	'Sitka Banner',
+	'Sitka Display',
+	'Sitka Heading',
+	'Sitka Small',
+	'Sitka Subheading',
+	'Sitka Text',
+	'Sylfaen',
+	'Symbol',
+	'Traditional Arabic',
+	'Trebuchet MS',
+	'Tunga',
+	'UD Digi Kyokasho',
+	'Urdu Typesetting',
+	'Utsaah',
+	'Vani',
+	'Verdana',
+	'Vijaya',
+	'Vrinda',
+	'Webdings',
+	'Wingdings',
+	'Yu Gothic',
+	'Yu Gothic UI',
+	'Yu Mincho',
+	'Al Bayan',
+	'American Typewriter',
+	'Andalé Mono',
+	'Apple Casual',
+	'Apple Chancery',
+	'Apple Garamond',
+	'Apple Gothic',
+	'Apple LiGothic',
+	'Apple LiSung',
+	'Apple Myungjo',
+	'Apple Symbols',
+	'.AquaKana',
+	'Arial Hebrew',
+	'Ayuthaya',
+	'Baghdad',
+	'Baskerville',
+	'Beijing',
+	'BiauKai',
+	'Big Caslon',
+	'Brush Script',
+	'Chalkboard',
+	'Chalkduster',
+	'Charcoal',
+	'Charcoal CY',
+	'Chicago',
+	'Cochin',
+	'Comic Sans',
+	'Cooper',
+	'Copperplate',
+	'Corsiva Hebrew',
+	'Courier',
+	'DecoType Naskh',
+	'Devanagari',
+	'Didot',
+	'Euphemia UCAS',
+	'Futura',
+	'Gadget',
+	'Geeza Pro',
+	'Geezah',
+	'Geneva',
+	'Geneva CY',
+	'Gill Sans',
+	'Gujarati',
+	'Gung Seoche',
+	'Gurmukhi',
+	'Hangangche',
+	'HeadlineA',
+	'Hei',
+	'Helvetica',
+	'Helvetica CY',
+	'Helvetica Neue',
+	'Herculanum',
+	'Hiragino Kaku Gothic Pro',
+	'Hiragino Kaku Gothic ProN',
+	'Hiragino Kaku Gothic Std',
+	'Hiragino Kaku Gothic StdN',
+	'Hiragino Maru Gothic Pro',
+	'Hiragino Maru Gothic ProN',
+	'Hiragino Mincho Pro',
+	'Hiragino Mincho ProN',
+	'Hoefler Text',
+	'Inai Mathi',
+	'Jung Gothic',
+	'Kai',
+	'Keyboard',
+	'Krungthep',
+	'KufiStandard GK',
+	'Kuenstler Script',
+	'LastResort',
+	'LiHei Pro',
+	'LiSong Pro',
+	'Lucida Sans',
+	'Marker Felt',
+	'Menlo',
+	'Monaco',
+	'Monaco CY',
+	'Mshtakan',
+	'Nadeem',
+	'New Peninim',
+	'New York',
+	'NISC GB18030',
+	'Optima',
+	'Osaka',
+	'Palatino',
+	'Papyrus',
+	'PC Myungjo',
+	'Pilgiche',
+	'Raanana',
+	'Sand',
+	'Sathu',
+	'Seoul',
+	'Shin Myungjo Neue',
+	'Silom',
+	'Skia',
+	'Snell Roundhand',
+	'ST FangSong',
+	'ST FangSong 2',
+	'ST Heiti',
+	'ST Kaiti',
+	'ST Song',
+	'Tae Graphic',
+	'Taipei',
+	'Techno',
+	'Textile',
+	'Thonburi',
+	'Times',
+	'Times CY',
+	'Zapf Chancery',
+	'Zapf Dingbats',
+	'Zapfino',
+];
+
+class CanvasKeyLogger{
+
+	constructor(options){
+
+		// The element to attach the listener to.
+		this.element = options.element || document;
+
+		// An array of characters that represent keys pressed
+		this.input = [];
+
+		// The current cursor position in the input array
+		this.cursor_pos = 0;
+
+		// Flag to indicate if the keylogger is logging keys or not
+		this.enabled = false;
+
+		// What event type to use for logging
+		this.event_type = options.event_type || 'keydown';
+
+		// The event handler that does all the work...
+		this._key_event_handler = this.key_event_handler.bind(this);
+
+		// Various optional user specified event listeners
+		this.on_input = (options.on_input || function(){}).bind(this);
+		this.on_nav_key = (options.on_nav_key || function(){}).bind(this);
+
+		// If there is an autostart option set to false, don't start logging immediately
+		if(!options.hasOwnProperty('autostart') || options.autostart !== false){
+			this.enable();
+		}
+
+	}
+
+	key_event_handler(e){
+
+		if(CanvasKeyLogger.NAMED_INPUT_KEYS[e.key]){
+			input.splice(this.cursor_pos, 0, CanvasKeyLogger.NAMED_INPUT_KEYS[e.key]);
+			this.cursor_pos++;
+			this.on_input();
+		}
+
+		else if(CanvasKeyLogger.CONTROL_KEYS.includes(e.key)){
+			switch(e.key){
+
+				case 'ArrowDown': 
+					this.on_nav_key();
+					break;
+
+				case 'ArrowLeft':
+					if(this.cursor_pos > 0){
+						this.cursor_pos--;
+					}
+					this.on_nav_key();
+					break;
+
+				case 'ArrowRight':
+					if(this.input.length > this.cursor_pos){
+						this.cursor_pos++;
+					}
+					this.on_nav_key();
+					break;
+
+				case 'ArrowUp':
+					this.on_nav_key();
+					break;
+
+				case 'End':
+					this.cursor_pos = this.input.length - 1;
+					this.on_nav_key();
+					break;
+
+				case 'Home':
+					this.cursor_pos = 0;
+					this.on_nav_key();
+					break;
+
+				case 'Backspace':
+					if(this.cursor_pos > 0){
+						this.input.splice(this.cursor_pos-1, 1);
+						this.cursor_pos--;
+						this.on_input();
+					}
+					break;
+
+				case 'Delete':
+					if(this.input.length > this.cursor_pos){
+						this.input.splice(this.cursor_pos, 1);
+						this.on_input();
+					}
+					break;
+
+			}
+		}
+
+		else if(!CanvasKeyLogger.NON_INPUT_KEYS.includes(e.key)){
+			this.input.splice(this.cursor_pos, 0, e.key);
+			this.cursor_pos++;
+			this.on_input();
+		}
+	}
+
+	val(as_array=false, cursor='CURSOR'){
+		if(as_array){
+			var value = [], i;
+			for(i=0; i < this.input.length; i++){
+				if(i == this.cursor_pos) value.push(cursor);
+				value.push(this.input[i]);
+			}
+			if(i == this.cursor_pos) value.push(cursor);
+			return value;
+		}else{
+			return this.input.join('');
+		}
+	}
+
+	enable(){
+		this.enabled = true;
+		this.element.addEventListener(this.event_type, this._key_event_handler);
+	}
+
+	disable(){
+		this.enabled = false;
+		this.element.removeEventListener(this.event_type, this._key_event_handler);
+	}
+
+}
+
+CanvasKeyLogger.NAMED_INPUT_KEYS = {Enter: "\n", Tab: "\t"};
+
+CanvasKeyLogger.CONTROL_KEYS = ['ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowUp',
+	'End', 'Home', 'Backspace', 'Delete'];
+
+CanvasKeyLogger.NON_INPUT_KEYS = ['Unidentified', 'Alt', 'AltGraph', 'CapsLock', 'Control',
+	'Fn', 'FnLock', 'Hyper', 'Meta', 'NumLock', 'ScrollLock', 'Shift', 'Super',
+	'Symbol', 'SymbolLock', 'PageDown', 'PageUp', 'Clear', 'Copy', 'CrSel',
+	'Cut', 'EraseEof', 'ExSel', 'Insert', 'Paste', 'Redo', 'Undo', 'Accept',
+	'Again', 'Attn', 'Cancel', 'ContextMenu', 'Escape', 'Execute', 'Find',
+	'Finish', 'Help', 'Pause', 'Play', 'Props', 'Select', 'ZoomIn', 'ZoomOut',
+	'BrightnessDown', 'BrightnessUp', 'Eject', 'LogOff', 'Power', 'PowerOff',
+	'PrintScreen', 'Hibernate', 'Standby', 'WakeUp', 'AllCandidates', 'Alphanumeric',
+	'CodeInput', 'Compose', 'Convert', 'Dead', 'FinalMode', 'GroupFirst', 'GroupLast',
+	'GroupNext', 'GroupPrevious', 'ModeChange', 'NextCandidate', 'NonConvert',
+	'PreviousCandidate', 'Process', 'SingleCandidate', 'HangulMode', 'HanjaMode',
+	'JunjaMode', 'Eisu', 'Hankaku', 'Hiragana', 'HiraganaKatakana', 'KanaMode',
+	'KanjiMode', 'Katakana', 'Romaji', 'Zenkaku', 'ZenkakuHanaku', 'F1',
+	'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12',
+	'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20', 'Soft1', 'Soft2',
+	'Soft3', 'Soft4', 'AppSwitch', 'Call', 'Camera', 'CameraFocus', 'EndCall',
+	'GoBack', 'GoHome', 'HeadsetHook', 'LastNumberRedial', 'Notification',
+	'MannerMode', 'VoiceDial', 'ChannelDown', 'ChannelUp', 'MediaFastForward',
+	'MediaPause', 'MediaPlay', 'MediaPlayPause', 'MediaRecord', 'MediaRewind',
+	'MediaStop', 'MediaTrackNext', 'MediaTrackPrevious', 'AudioBalanceLeft',
+	'AudioBalanceRight', 'AudioBassDown', 'AudioBassBoostDown', 'AudioBassBoostToggle',
+	'AudioBassBoostUp', 'AudioBassUp', 'AudioFaderFront', 'AudioFaderRear',
+	'AudioSurroundModeNext', 'AudioTrebleDown', 'AudioTrebleUp', 'AudioVolumeDown',
+	'AudioVolumeMute', 'AudioVolumeUp', 'MicrophoneToggle', 'MicrophoneVolumeDown',
+	'MicrophoneVolumeMute', 'MicrophoneVolumeUp', 'TV', 'TV3DMode', 'TVAntennaCable',
+	'TVAudioDescription', 'TVAudioDescriptionMixDown', 'TVAudioDescriptionMixUp',
+	'TVContentsMenu', 'TVDataService', 'TVInput', 'TVInputComponent1', 'TVInputComponent2',
+	'TVInputComposite1', 'TVInputComposite2', 'TVInputHDMI1', 'TVInputHDMI2',
+	'TVInputHDMI3', 'TVInputHDMI4', 'TVInputVGA1', 'TVMediaContext', 'TVNetwork',
+	'TVNumberEntry', 'TVPower', 'TVRadioService', 'TVSatellite', 'TVSatelliteBS',
+	'TVSatelliteCS', 'TVSatelliteToggle', 'TVTerrestrialAnalog', 'TVTerrestrialDigital',
+	'TVTimer', 'AVRInput', 'AVRPower', 'ColorF0Red', 'ColorF1Green', 'ColorF2Yellow',
+	'ColorF3Blue', 'ColorF4Grey', 'ColorF5Brown', 'ClosedCaptionToggle',
+	'Dimmer', 'DisplaySwap', 'DVR', 'Exit', 'FavoriteClear0', 'FavoriteClear1',
+	'FavoriteClear2', 'FavoriteClear3', 'FavoriteRecall0', 'FavoriteRecall1',
+	'FavoriteRecall2', 'FavoriteRecall3', 'FavoriteStore0', 'FavoriteStore1',
+	'FavoriteStore2', 'FavoriteStore3', 'Guide', 'GuideNextDay', 'GuidePreviousDay',
+	'Info', 'InstantReplay', 'Link', 'ListProgram', 'LiveContent', 'Lock',
+	'MediaApps', 'MediaAudioTrack', 'MediaLast', 'MediaSkipBackward', 'MediaSkipForward',
+	'MediaStepBackward', 'MediaStepForward', 'MediaTopMenu', 'NavigateIn',
+	'NavigateNext', 'NavigateOut', 'NavigatePrevious', 'NextFavoriteChannel',
+	'NextUserProfile', 'OnDemand', 'Pairing', 'PinPDown', 'PinPMove', 'PinPToggle',
+	'PinPUp', 'PlaySpeedDown', 'PlaySpeedReset', 'PlaySpeedUp', 'RandomToggle',
+	'RcLowBattery', 'RecordSpeedNext', 'RfBypass', 'ScanChannelsToggle',
+	'ScreenModeNext', 'Settings', 'SplitScreenToggle', 'STBInput', 'STBPower',
+	'Subtitle', 'Teletext', 'VideoModeNext', 'Wink', 'ZoomToggle', 'SpeechCorrectionList',
+	'SpeechInputToggle', 'Close', 'New', 'Open', 'Print', 'Save', 'SpellCheck',
+	'MailForward', 'MailReply', 'MailSend', 'LaunchCalculator', 'LaunchCalendar',
+	'LaunchContacts', 'LaunchMail', 'LaunchMediaPlayer', 'LaunchMusicPlayer',
+	'LaunchMyComputer', 'LaunchPhone', 'LaunchScreenSaver', 'LaunchSpreadsheet',
+	'LaunchWebBrowser', 'LaunchWebCam', 'LaunchWordProcessor', 'LaunchApplication1',
+	'LaunchApplication2', 'LaunchApplication3', 'LaunchApplication4', 'LaunchApplication5',
+	'LaunchApplication6', 'LaunchApplication7', 'LaunchApplication8', 'LaunchApplication9',
+	'LaunchApplication10', 'LaunchApplication11', 'LaunchApplication12', 'LaunchApplication13',
+	'LaunchApplication14', 'LaunchApplication15', 'LaunchApplication16', 'BrowserBack',
+	'BrowserFavorites', 'BrowserForward', 'BrowserHome', 'BrowserRefresh', 'BrowserSearch',
+	'BrowserStop', 'Decimal', 'Key11', 'Key12', 'Multiply', 'Add', 'Clear',
+	'Divide', 'Subtract', 'Separator'
+];
+
+class TypingCanvas extends DrawingCanvas{
+	
+	constructor(canvas, options){
+		this.active = false;
+		this.text = '';
+		this.canvas = canvas;
+		
+		this.type_area = {};
+		this.type_area.width = options.type_area.width || canvas.width;
+		this.type_area.height = options.type_area.height || canvas.height;
+		this.type_area.x = options.type_area.x || 0;
+		this.type_area.y = options.type_area.y || 0;
+		
+		this._render_canvas = document.createElement('canvas');
+		this._render_canvas.width = this.type_area.width;
+		this._render_canvas.height = this.type_area.height;
+		
+		this.last_render_background = null;
+		
+		this._keydownHandler = this.keydownHandler.bind(this);
+		this.attachEventHandlers();
+	}
+	
+	setTypeArea(x, y, w, h){
+		this.type_area.width = w;
+		this.type_area.height = h;
+		this.type_area.x = x;
+		this.type_area.y = y;
+	}
+	
+	keydownHandler(){
+		
+	}
+	
+	attachEventHandlers(){
+		this.canvas.addEventListener(this._keydownHandler);
+	}
+	
+	removeEventHandlers(){
+		this.canvas.removeEventListener(this._keydownHandler);
 	}
 	
 }
